77cafe10ac74df6c3d69c9cce6e9988e340019d5
Desi Sussu <desiremrv@gmail.com>
2023-06-12 19:06:13 +0200

    feat(ui): create component 'What it is about'

---
+++ src/components/WhatAbout.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const WhatAbout = () => {
+  return (
+    
+    <div className='flex-col bg-background_Blue h-screen md:px-20 xl:px-auto'>
+        <h1 className='text-cyan-950 text-center font-black text-5xl'>What it is about</h1>
+        <div className='flex'>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel pretium felis. Morbi ac felis blandit, tristique erat vel, tristique quam.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel pretium felis. Morbi ac felis blandit, tristique erat vel, tristique quam.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel pretium felis. Morbi ac felis blandit, tristique erat vel, tristique quam.</p>
+            </div>
+            <img src=''></img>
+        </div>
+    </div>
+    
+  )
+}
+
+export default WhatAbout

f2123a942b1d5433d455a845bec43dd614000e99
juanpablo1978 <jpandrujovich@gmail.com>
2023-06-14 19:37:35 -0300

    cambios hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Hero from './components/Hero'
+import Footer from './layout/Footer'
 
 function App() {
 
 
   return (
+    <>
     <Hero/>
+    <Footer/>
+    </>
   )
 }
 

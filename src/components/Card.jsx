f39ef8d792887d8dd58431871276245efdba84e8
Desi Sussu <desiremrv@gmail.com>
2023-06-16 17:29:56 +0200

    feat(ui): Card layout completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,19 +4,19 @@ import image from '../assets/images/howl.jpg'
 export const Card = () => {
   return (
     <div className=' flex justify-center items-center w-screen h-screen'>
-        <div className=' flex-col w-3/12 h-100  rounded shadow-lg '>
+        <div className=' flex-col w-3/12 h-auto  rounded shadow-lg '>
             <div className='w-full h-50 '> <img src={image} className='rounded-t' /></div>
-            <div className='m-6'>
-                <span className='text-lg'>El castillo ambulante | ハウルの動く城</span>
-                <div>
-                    <p><span className='text-base'> Running time: </span> 119 min</p>
-                    <p><span className='text-base'>Directed by:</span>Hayao Miyazaki</p>
-                    <p><span className='text-base'>Produced by:</span>  Toshio Suzuki | Hayao Miyazaki | Tomohiko Ishii</p>
+            <div className='m-5'>
+                <span className='text-lg font-medium'>El castillo ambulante | ハウルの動く城</span>
+                <div className='text-base font-light py-4'>
+                    <p><span className='font-normal'> Running time: </span> 119 min</p>
+                    <p><span className='font-normal'>Directed by: </span>Hayao Miyazaki</p>
+                    <p><span className='font-normal'>Produced by: </span>  Toshio Suzuki | Hayao Miyazaki | Tomohiko Ishii</p>
                 </div>
-                <div className='flex gap-3 text-xs'>
-                <div>#gibliStudio</div>
-                <div>#Miyazaki</div>
-                <div>#anime</div>
+                <div className='flex gap-3 text-xs my-8'>
+                <div className='cursor-pointer bg-gray-300 text-gray-700 px-4 py-1 rounded-full'>#gibliStudio</div>
+                <div className='cursor-pointer bg-gray-300 text-gray-700 px-4 py-1 rounded-full'>#Miyazaki</div>
+                <div className='cursor-pointer bg-gray-300 text-gray-700 px-4 py-1 rounded-full'>#anime</div>
             </div>
             </div>
         </div>

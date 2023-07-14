77127a13acc5a441ec94d8a4b0c1e499ec4fb873
Agustin Rossello <agustinrossello@gmail.com>
2023-07-14 16:20:51 -0300

    Navbar inteligente incompleto

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   const pages = [
@@ -8,9 +9,23 @@ const Header = () => {
     { name: "TEAM", path: "/team" },
   ];
 
+  const detectPath = () => {
+    const currentPath = window.location.pathname
+    return currentPath
+  }
+
+  const [currentPath, setCurrentPath] = useState("")
+
+  useEffect(() => {
+    setCurrentPath(detectPath())
+  }, [setCurrentPath])
+  
+
   return (
-    <header className="flex items-center absolute z-10 top-0 left-0 w-full  h-[113px] text-[#fafafa]">
-      {/* Puse un fondo black pero en realidad va transparent */}
+
+    <header className= {`flex items-center absolute z-10 top-0 left-0 w-full  h-[113px]
+    
+    ${ currentPath === "/" ? "text-[#fafafa]" : currentPath === "/docs" ? "text-[#0E7490]" : "" }`}>
       <div className="max-w-[1366px] mx-auto">
         <nav className="flex justify-center items-center w-full h-full">
           <h1 className="font-bold text-[30px] ">
@@ -19,8 +34,13 @@ const Header = () => {
           <ul className="flex gap-10 mr-60 px-24 font-helvetica relative">
             {pages.map((page, index) => (
               <li className="group font-medium text-[22px]  " key={index}>
-                <Link to={page.path}> {page.name} </Link>
+                <NavLink 
+                  to={page.path} 
+                  className={({isActive}) => isActive && detectPath() === "/" ? (console.log(detectPath()), "bg-purple-900 rounded-md p-1") 
+                  : isActive && detectPath() === "/docs" ? (console.log(detectPath()), "bg-[#083344] rounded-md p-1 text-white") : ""} > {page.name}
 
+                  {/* En caso de agregarse mas rutas con distintos estilos de navbar, es acá donde hay que trabajar */}
+                </NavLink>
                 <div
                   className={`
                 ${
@@ -36,15 +56,17 @@ const Header = () => {
                 }    `}
                 ></div>
               </li>
+              
             ))}
           </ul>
 
-          <button className="font-helvetica font-semibold text-[20px] border-2 rounded-full p-2 px-6 ">
+          <button className="font-helvetica font-normal text-[18px] border rounded-full p-2 px-6 ">
             SUPPORT US
           </button>
         </nav>
       </div>
     </header>
+
   );
 };
 

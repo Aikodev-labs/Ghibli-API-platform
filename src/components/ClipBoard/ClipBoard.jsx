46be21c6768ceb82c885750f7a9e4e099c4e0883
juanpablo1978 <jpandrujovich@gmail.com>
2023-12-01 17:30:11 -0300

    codigo de caja de script

--- src/components/ClipBoard/ClipBoard.jsx
+++ src/components/ClipBoard/ClipBoard.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { HiOutlineClipboardDocument } from "react-icons/hi2";
 
 
 const ClipBoard = ( ) => {
 
   return (
 
-  <CopyToClipboard text=''>
+    <section className='w-[510px] h-[43px] bg-white flex justify-between items-center px-[15px] rounded-[4px]'>
 
-    </CopyToClipboard>
+    <p className='text-[16px] text-cyan-700'>
+      <span className='font-bold border-b-[1px] border-b-cyan-700'>https:/</span>
+      <span className='font-normal'>ghibliapi.com/api</span></p>
+    
+      <CopyToClipboard text='https:/ghibliapi.com/api'>
+    
+        <button>
+          <HiOutlineClipboardDocument className='text-gray-600 w-[18px] h-[18px]' />
+        </button>
+    
+        </CopyToClipboard>
+    
+      </section>
 
     
   )

57b2abd8d71266f43f4136eb40ffc0a40741d7c2
juanpablo1978 <jpandrujovich@gmail.com>
2023-12-01 07:46:49 -0300

    componente ClipBoard

---
+++ src/components/ClipBoard/ClipBoard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+
+const ClipBoard = ( ) => {
+
+  return (
+
+  <CopyToClipboard text=''>
+
+    </CopyToClipboard>
+
+    
+  )
+}
+
+export default ClipBoard

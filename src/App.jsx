d48a22ec58ba8235beac83703c03c1cf43fd1716
Josue Oroya <joshuacba08@gmail.com>
2025-06-14 00:06:07 -0300

    Refactor App component by reorganizing imports and cleaning up code structure for improved readability.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,30 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 //Components
-import Home from "./pages/Home"
-import Header from "./layout/Header";
 import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+import Home from "./pages/Home";
 //Pages
 import About from "./pages/About";
+import Support from "./pages/Support";
 import Team from "./pages/Team";
 import DocRouter from "./router/doc.router";
-import Support from "./pages/SupporT.JSX";
-
-
-
-
-
-
-
-
-
 
 function App() {
-
-  
   return (
     <BrowserRouter>
-
       <Header />
-      
 
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/docs/*"   element={ <DocRouter /> } />
-        <Route path="/about" element={<About />}/>
-        <Route path="/team" element={<Team />}/>
-        <Route path="/support" element={<Support />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/docs/*" element={<DocRouter />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/team" element={<Team />} />
+        <Route path="/support" element={<Support />} />
       </Routes>
 
       <Footer />
-      
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
- 
+export default App;

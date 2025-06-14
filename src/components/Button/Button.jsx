bbda2ba8efad9e15af967044bb77adfc943c1dd5
Josue Oroya <joshuacba08@gmail.com>
2025-06-13 23:12:38 -0300

    Add PropTypes to Button component and enhance Support page with status-based alerts and navigation buttons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const Button = ({ text, type, size, onClick, disabled }) => {
   const getType = () => {
     switch (type) {
@@ -6,7 +8,7 @@ const Button = ({ text, type, size, onClick, disabled }) => {
       case "outline":
         return "outline outline-1 outline-white text-white"; /*Sin fondo solo de linea */
       case "outline_blue":
-        return "outline outline-1 outline-cyan700 text-cyan700"; /*Sin fondo solo de linea */   
+        return "outline outline-1 outline-cyan700 text-cyan700"; /*Sin fondo solo de linea */
       case "secondary":
         return "bg-teal-600 text-white hover:bg-teal-700"; /*Boton verde*/
       default:
@@ -26,17 +28,23 @@ const Button = ({ text, type, size, onClick, disabled }) => {
         return "text-sm";
     }
   };
- const diseable = ()=>{
-  return diseable
- }
 
   const buttonClasses = `${getType()} ${getSize()} rounded-full text-center py-2 justify-center items-center inline-flex font-neue-lt `;
 
   return (
     /*Ejemplo de uso <Button text='TEXTO' type="primary" size="s"></Buttton>*/
 
-    <button className={buttonClasses} onClick={onClick} disabled={disabled} >{text}</button>
+    <button className={buttonClasses} onClick={onClick} disabled={disabled}>
+      {text}
+    </button>
   );
 };
 export default Button;
 
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
